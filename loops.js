7dc16557d951408c8d3ae71566bd484b26b69b72
Luoana Chirita <luoana.livadariu@gmail.com>
2018-06-13 12:48:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array) {
   for (let i = 0; i <25; i++){
     if (i === 1) {
-      array[i] = "I am 1 strange loop.";
+      array.splice(i, 0, "I am 1 strange loop.")
     }
     else {
       array.push("I am ${i} strange loops.");
